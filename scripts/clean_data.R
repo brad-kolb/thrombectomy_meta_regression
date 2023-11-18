bd0a45030f8d8efa7e9ab0eb57b30ac29f63ad61
Brad Kolb <bradkolb@gmail.com>
2023-11-18 11:15:10 -0600

    clean

---
+++ scripts/clean_data.R
@@ -0,0 +1,49 @@
+#### preamble ####
+# purpose: clean manually collected thrombectomy trial data
+# author: Bradley Kolb
+# date: 18-November-2023
+# contact: bradkolb@gmail.com
+
+#### work space setup ####
+# load packages
+library(here)
+library(tidyverse)
+
+# set input file
+# this data was manually entered into a spreadsheet by me
+file_in <- here("data/data_raw", "raw_data.csv")
+
+# set output files
+# independent defined as mrs 0, 1, or 2
+file_out <- here("data/data_processed", "independent.csv")
+
+# read in data
+data_raw <- read_csv(
+  file = file_in
+)
+
+# make tibble for independent
+# these definitions follow the nomenclature from Gelman et al., Bayesian Data Analysis
+independent <- tibble(
+  j = data_raw %>% 
+    filter(treatment_id == 1) %>% 
+    .$trial_id, 
+  n_0j = data_raw %>% 
+    filter(treatment_id == 0) %>% 
+    .$tot_actual,
+  n_1j = data_raw %>% 
+    filter(treatment_id ==1) %>% 
+    .$tot_actual,
+  y_0j = data_raw %>% 
+    filter(treatment_id == 0) %>% 
+    .$ind_mrs,
+  y_1j = data_raw %>% 
+    filter(treatment_id == 1) %>% 
+    .$ind_mrs,
+)
+
+independent <- independent %>% 
+  mutate(
+    y_j = log(y_1j / (n_1j - y_1j)) - log(y_0j / (n_0j - y_0j)),
+    sigma_j = 1/y_1j + 1/(n_1j - y_1j) + 1/y_0j + 1/(n_0j - y_0j)
+  )
